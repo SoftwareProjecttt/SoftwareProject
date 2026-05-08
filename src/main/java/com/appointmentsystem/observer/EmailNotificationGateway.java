6facf0b7ee5e6146d736248355d0d092c16f988c
كرم عمار غازي صلاحات <s12217026@stu.najah.edu>
2026-05-09 00:51:20 +0300

    Replace System.out with Logger for email notifications

--- src/main/java/com/appointmentsystem/observer/EmailNotificationGateway.java
+++ src/main/java/com/appointmentsystem/observer/EmailNotificationGateway.java
@@ -2,11 +2,15 @@ package com.appointmentsystem.observer;
 
 import com.appointmentsystem.NotificationGateway;
 
+import java.util.logging.Logger;
+
 public class EmailNotificationGateway implements NotificationGateway {
+    private static final Logger LOGGER = Logger.getLogger(EmailNotificationGateway.class.getName());
+
     @Override
     public void send(String email, String message) {
-        System.out.println("[EMAIL SENT]");
-        System.out.println("To: " + email);
-        System.out.println("Message: " + message);
+        LOGGER.info("[EMAIL SENT]");
+        LOGGER.info("To: " + email);
+        LOGGER.info("Message: " + message);
     }
 }
